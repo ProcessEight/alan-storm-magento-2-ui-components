b0503d0dd9bb845a67fb4fcbd7912137f9a5a21d
ProjectEight <s.frost@frostnet.co.uk>
2017-03-17 16:18:25 +0000

    Updated README; Added module skeleton

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Work related to the [Mage2Katas](https://www.youtube.com/channel/UCRFDWo7jTlrpEsJxzc7WyPw) series.
 
+## The Module Skeleton Kata
+
+## The Plugin Config Kata
+
+## The Around Interceptor Kata
---
+++ app/code/Mage2Kata/Interceptor/Test/Unit/Plugin/CustomerRepositoryPluginTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zone8
+ * Date: 17/03/17
+ * Time: 16:23
+ */
+
+namespace Mage2Kata\Interceptor\Plugin;
+
+
+class CustomerRepositoryPluginTest extends \PHPUnit_Framework_TestCase
+{
+	public function testEnvironment()
+	{
+		$this->assertSame( 0, 0);
+	}
+}
